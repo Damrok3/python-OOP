4c37a176e4eb61cd60884894f8a958c3b1eea77a
Damrok <ckm7667@gmail.com>
2023-07-14 10:04:18 +0200

    bro code course

---
+++ Bro code/strings.py
@@ -0,0 +1,12 @@
+name = "bro code"
+
+print(len(name))
+print(name.find("r"))
+print (name.capitalize())
+print (name.upper())
+print (name.lower())
+print (name.isdigit())
+print (name.isalpha())
+print (name.count("o"))
+print (name.replace("o","a"))
+print (name*3)
