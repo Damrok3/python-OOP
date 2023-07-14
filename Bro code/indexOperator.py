bfa7f9bf97a806c7e7eee2d04dc3dd36966b707c
Damrok <ckm7667@gmail.com>
2023-07-14 13:19:14 +0200

    index operator

---
+++ Bro code/indexOperator.py
@@ -0,0 +1,13 @@
+# index operator [] = gives access to a squence's element (str,list,tuples)
+
+name = "bro Code"
+
+# if(name[0].islower()):
+#     name = name.capitalize()
+
+first_name = name[0:3].upper()
+last_name = name[4:].lower()
+last_character = name[-1]
+
+print(first_name, last_name, last_character)
+
