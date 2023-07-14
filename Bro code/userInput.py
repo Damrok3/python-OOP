94d9dec22b72eeff18e49247c7b3889412a6db87
Damrok <ckm7667@gmail.com>
2023-07-14 10:17:35 +0200

    user input

---
+++ Bro code/userInput.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("how tall are you?: "))
+
+age = age + 1
+
+print("Hello " + name)
+print("you are " + str(age) + " years old")
+print("You are " + str(height)+ " cm tall")
