ef37d207daa66fbd085c747a374bae90650fe14e
Damrok <ckm7667@gmail.com>
2023-07-14 10:23:04 +0200

    math functions

---
+++ Bro code/mathFunctions.py
@@ -0,0 +1,22 @@
+import math
+
+pi = -3.14
+x = 1
+y = 3
+z = 2
+
+L = [x,y,z]
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+
+pi = pi * -1
+
+print(pow(pi,2))
+print(math.sqrt(420))
+print(max(L))
+print(min(x,y,z))
+
+
