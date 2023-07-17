fbb8ddcf7c6922b0766b2e15a4f492abc534854b
Damrok <ckm7667@gmail.com>
2023-07-17 10:18:58 +0200

    functions

---
+++ Bro code/functions.py
@@ -0,0 +1,10 @@
+# function = a block of code which is executed only when it is called.
+
+def hello(name, lastname, age):
+    print("hello " + name + " " + lastname)
+    print("you are "+ str(age)+ " years old")
+    print("have a nice day")
+
+my_name = "Bro"
+
+hello(my_name, "Code", 23)
