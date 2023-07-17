7cfbdc7572f18edc351424ac39e9c046b1beedad
Damrok <ckm7667@gmail.com>
2023-07-17 10:46:38 +0200

    kwargs

--- Bro code/args.py
+++ Bro code/args.py
@@ -1,7 +1,7 @@
 # *args = paremeter that will pack all arguments into a tuple
 #         useful so that a function can accept a varying amount of arguments
 
-def add(*stuff):
+def add(*stuff): #the convention is to name it args but it's not necesarry
     sum = 0
     stuff = list(stuff)
     stuff[0] = 0
