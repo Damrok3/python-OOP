7b334fdf333d708a47bf6cacb5722931b433b35a
Damrok <ckm7667@gmail.com>
2023-07-21 12:45:01 +0200

    randomNumbers

---
+++ Bro code/randomNumbers.py
@@ -0,0 +1,14 @@
+import random
+
+x = random.randint(1,6)
+y = random.random()     #this gives random float between 0 and 1
+
+myList = ["rock", "paper", "scissors"]
+z = random.choice(myList)
+
+cards = [1,2,3,4,5,6,7,8,9,"J","Q","K","A"]
+
+random.shuffle(cards)
+
+print(z)
+print(cards)
