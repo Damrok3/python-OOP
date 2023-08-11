bfad64161fe48fdef8fea359110660e3b5cbc79f
Damrok <ckm7667@gmail.com>
2023-08-11 17:53:21 +0200

    text entry box

---
+++ Bro code/GUIwindows/entrybox.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+# entry widget = textbox that accepts a single line of user input
+
+def submit():
+    username = entry.get()
+    print("Hello "+username)
+
+def delete():
+    entry.delete(0, END)
+
+def backspace():
+    entry.delete(len(entry.get()) - 1,END)
+
+window = Tk()
+
+entry = Entry(window,
+              font = ("Blambot Pro Lite", 50),
+              fg="#00FF00",
+              bg="black")
+entry.insert(0, "type text here")
+#entry.config(state=DISABLED)
+#entry.config(show="*")
+entry.pack(side=LEFT)
+
+submit_button = Button(window, text = "submit", command=submit)
+submit_button.pack(side=RIGHT)
+
+delete_button = Button(window, text = "delete", command=delete)
+delete_button.pack(side=RIGHT)
+
+backspace_button = Button(window, text = "backspace", command=backspace)
+backspace_button.pack(side=RIGHT)
+
+window.mainloop()
