fc01aa78e7da2157deb2d469a7c0b48183efe727
Damrok <ckm7667@gmail.com>
2023-08-11 19:43:22 +0200

    sliding scale

---
+++ Bro code/GUIwindows/slidingscale.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+def submit():
+    print("the temperatur is: " + str(scale.get())+" degrees C")
+
+window = Tk()
+
+flameimg = PhotoImage(file="flame.png")
+flameLabel = Label(image=flameimg)
+flameLabel.pack()
+
+scale = Scale(window,
+              from_= 100,
+              to = 0,
+              length=600,
+              orient=VERTICAL, # orientation of scale
+              font=("Blambot Pro Lite", 20),
+              tickinterval=10, # adds numeric indicators for value
+              showvalue=1,
+              resolution=5, # increment of slider
+              troughcolor="#69eaff",
+              fg="#ff1c00",
+              bg="black")
+
+
+scale.set((scale["from"] - scale["to"]) / 2 + scale["to"])
+scale.pack()
+
+snowimg = PhotoImage(file="snow.png")
+snowLabel = Label(image=snowimg)
+snowLabel.pack()
+
+button = Button(window, text="submit", command=submit)
+button.pack()
+
+window.mainloop()
+
