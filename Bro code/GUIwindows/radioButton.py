7e69c0b56b0b8ee308fdbdea793ee5888ad3cd70
Damrok <ckm7667@gmail.com>
2023-08-11 18:50:56 +0200

    radiobuttons

---
+++ Bro code/GUIwindows/radioButton.py
@@ -0,0 +1,43 @@
+# radio button = similar to checkbox, but you can only select one from a group
+
+from tkinter import *
+
+food = ["pizza", "hamburger"," hotdog"]
+
+def order():
+    if(x.get() == 0):
+        print("you ordered pizza")
+    elif(x.get() == 1):
+        print("you ordered a hamburger")
+    elif(x.get() == 2):
+        print("you ordered a hotdog")
+    else:
+        print("huh")
+
+
+window = Tk()
+
+pizzaImage = PhotoImage(file="pizza.png")
+hamburgerImage = PhotoImage(file="hamburger.png")
+hotdogImage = PhotoImage(file="hotdog.png") 
+foodImages = [pizzaImage, hamburgerImage, hotdogImage]
+
+x = IntVar()
+
+for index in range(len(food)):
+    radiobutton = Radiobutton(window,
+                              text=food[index], # adds text to radio buttons
+                              variable=x, # groups radiobuttons together if they share the same variable
+                              value=index, # assigns each radiobutton a different value
+                              padx=25, # adds padding on x axis
+                              font=("Blambot Pro Lite", 20),
+                              image=foodImages[index], #adds image to radiobutton
+                              compound=LEFT, # adds image & text (left-side)
+                              indicatoron=0, #eliminate circle indicators
+                              width=375, # sets width of radiobuttons
+                              command=order, # set command of radiobutton to function
+                              )
+    
+    radiobutton.pack(anchor=W)
+
+window.mainloop()
