38a41be2a14f8d01cadb7654447c42f8543ad271
Damrok <ckm7667@gmail.com>
2023-08-16 21:32:01 +0200

    run in cmd

---
+++ Bro code/run_in_cmd.py
@@ -0,0 +1,11 @@
+# run .py file in cmd
+
+# save file as .py (python file)
+# go to command prompt
+# navigate to directory w/ your file: cd C:\users\username\...
+# invoke python interpreter + script: python hello_world.py
+
+print("hello world")
+
+name = input("what's your name?: ")
+print("hello" + name)
